d294d22a5db49bbd4f625ba0005676c117745642
eisterman <federico.pasqua.96@gmail.com>
2017-06-20 02:46:14 +0200

    Compilatore inserito, migliorato funzionamento e suddivisione tra Environment e Compiler. Primo test con dichiarazione variabile su index 0 riuscito

--- BFPC.py
+++ BFPC.py
@@ -1,10 +1,64 @@
-from abc import ABC
+from abc import ABC,abstractmethod
+from collections import OrderedDict
+
+
+class Environment:
+    def __init__(self):
+        self.PseudoCode = [] # Contenitore delle operazioni da eseguire
+        self.StackObject = OrderedDict() # Container degli StackObj
+        self.HeapObject = {} # Container degli HeapObj
+    def insertOp(self,operation): # Inserisce operazione nello PseudoCode
+        self.PseudoCode.append(operation)
+    # Quando si va in compilazione si usa la posizione in StackObject e HeapObject per avere la cella
+    def preCompile(self):
+        for op in self.PseudoCode:
+            op.preComp(self)
+        #Measure Stack Size
+        ssize = 0
+        for obs in self.StackObject.values():
+            ssize += obs._byte
+        self.StackSize = ssize
+        #TODO: Measure REG Size
+        #TODO: Measure Heap Size ma attenzione che vengono trattati dinamicamente quindi in compile time
+
+class Compiler:
+    def __init__(self,env:Environment):
+        self._env = env
+        self._code = ""
+    def compile(self):
+        self._code = ""
+        pointer = 0
+        for op in self._env.PseudoCode:
+            code,newpointer = op.compute(self._env,pointer)
+            self._code += code
+            pointer = newpointer
+        return self._code
 
 class Operation(ABC): # Operazione eseguibile e compilabile in BF
     pass
 
 class VarOp(Operation): # Operazione di creazione di una nuova istanza STATICA
-    pass
+    def __init__(self,ID,obj):
+        #TODO: Assert the world
+        self._id = ID
+        self._value = obj.getValue()
+        self._byte = obj.getByte()
+    def preComp(self, comp : Environment): #Può modificare lo stato del compilatore!
+        if self._id in comp.StackObject.keys():
+            raise "Duplicated Creation!"
+        else:
+            comp.StackObject[self._id] = StackObj(self._value,self._byte)
+    def compute(self,env : Environment,p : int):
+        #CASO SPECIALE UN BYTE:
+        #TODO: Estensione a multipli Byte
+        code = ""
+        target = int(list(env.StackObject).index(self._id))
+        if p > target:
+            code += "<"*(p-target)
+        else:
+            code += ">"*(target-p)
+        code += "+"*self._value
+        return (code,target)
 
 class NewOp(Operation): # Operazione di creazione di una nuova istanza DINAMICA
     pass
@@ -22,23 +76,42 @@ class MemObj(ABC): # Oggetto rappresentante una cella di memoria
     pass
 
 class StackObj(MemObj): # Oggetto rappresentate variabile statica in Stack
-    pass
+    def __init__(self,value,byte):
+        self._value = value
+        self._byte = byte
+        #TODO: Error handling
+    #TODO: Property
+
+class RegObj(MemObj): # Oggetto rappresentante i registri temporanei
+    pass #Forse non serve neanche sta classe
 
 class HeapOnbj(MemObj): # Oggetto rappresentante variabile dinamica in Heap
     pass
 
-class Compiler:
-    def __init__(self):
-        self.PseudoCode = [] # Contenitore delle operazioni da eseguire
-        #self.StackObject = {} # Container degli StackObj
-        #self.HeapObject = {} # Container degli HeapObj
-    def infereStackSize(self): # Calcola dimensione dello stack
+class InterfaceObj(ABC): # Oggetti di interfaccia con l'esterno
+    @abstractmethod
+    def getValue(self):
         pass
-    def infereREGSize(self): # Calcola numero di registri necessari per fare le cose.
+    @abstractmethod
+    def getByte(self):
         pass
-    def insertOp(self,operation): # Inserisce operazione nello PseudoCode
-        pass
-    def getBFCodeOp(self,operation): # Ottiene il codice BF da un operazione (PRIVATE)
-        pass
-    def compile(self):
+
+class Byte(InterfaceObj):
+    def __init__(self,value):
+        self._val = int(value)
+        self._byte = 1
+        #TODO: Value Handling
+    def getByte(self):
+        return self._byte
+    def getValue(self):
+        return self._val
+
+class OperationManager:
+    def __init__(self,comp):
+        assert isinstance(comp, Environment)
+        self._compiler = comp
+    def newStatic(self,name,obj):
+        op = VarOp(name,obj)
+        self._compiler.insertOp(op)
+    def getOpBF(self):
         pass
--- demoBFPC.py
+++ demoBFPC.py
@@ -1,7 +1,12 @@
-
+from BFPC import Environment,OperationManager,Byte,Compiler
 
 def main():
-    pass
-
+    executor = Environment()
+    helper = OperationManager(executor)
+    helper.newStatic("ciao",Byte(5))
+    executor.preCompile()
+    comp = Compiler(executor)
+    code = comp.compile()
+    print(code)
 
 if __name__ == '__main__': main()
