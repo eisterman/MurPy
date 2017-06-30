4d3e741e30822f1aff038f8933f057cdd762dcee
eisterman <federico.pasqua.96@gmail.com>
2017-06-30 20:15:15 +0200

    Introduced new InterfaceObj and protovar interface command for the start of the total refactorization of the project

--- _interfaceobjects.py
+++ _interfaceobjects.py
@@ -1,4 +1,5 @@
 from abc import ABC,abstractmethod
+from _operations import newOperation
 
 class InterfaceObj(ABC): # Oggetti di interfaccia con l'esterno
     @abstractmethod
@@ -17,3 +18,22 @@ class Byte(InterfaceObj):
         return self._byte
     def getValue(self):
         return self._val
+
+# Nuovi Metodi di Interfaccia
+class newInterfaceObj(ABC):
+    buffer = []
+    @classmethod
+    def getBuffer(cls):
+        tmp = cls.buffer
+        cls.buffer = []
+        return tmp
+    @abstractmethod
+    def __call__(self, *args, **kwargs):
+        """Devi passargli tutti i parametri del comando di interfaccia."""
+        pass
+
+class protovar(newInterfaceObj):
+    def __call__(self, name, value, typecode='ub'): #TODO: Typecoding
+        # PER ORA SOLO VALORI NUMERICI PURI
+        # TODO: Controllo per i Typecode
+        pass
