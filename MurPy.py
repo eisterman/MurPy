47aecb22d60172c5b01bc6f77751f6359d2b27d1
eisterman <federico.pasqua.96@gmail.com>
2017-06-30 23:16:13 +0200

    newEnvironment received method addRoutine, Parse and attribute RoutineDict. Implemented new newEnvironment.Compile method.

--- MurPy.py
+++ MurPy.py
@@ -2,6 +2,8 @@ from abc import ABC,abstractmethod
 from collections import OrderedDict
 from _interfaceobjects import Byte
 from _operations import VarOp, SetOp
+
+from _interfaceobjects import newInterfaceObj
 #TODO: Index for variable name
 class Environment:
     def __init__(self):
@@ -52,8 +54,16 @@ class OperationManager:
 # New Environment
 class newEnvironment:
     def __init__(self):
+        self._code = None
         self.PseudoCode = [] # Contenitore delle operazioni da eseguire
         self.StackObject = OrderedDict()  # Container degli StackObj
+        self.RoutineDict = {}
+    def addRoutine(self,func):
+        self.RoutineDict[func.__name__] = func
+    def Parse(self):
+        # MODELLO PER UNA SOLA FUNZIONE MAIN
+        self.RoutineDict["main"]()
+        self.PseudoCode = newInterfaceObj.getBuffer()
     def Precompile(self):
         for op in self.PseudoCode:
             op.PreCompile(self)
@@ -64,4 +74,13 @@ class newEnvironment:
         self.StackSize = ssize
         # TODO: Measure REG Size
     def Compile(self):
-        pass
+        self._code = ""
+        pointer = 0
+        for op in self.PseudoCode:
+            code, newpointer = op.GetCode(self, pointer)
+            self._code += code
+            pointer = newpointer
+        return self._code
+    @property
+    def Code(self):
+        return self._code
