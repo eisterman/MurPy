dd9a2c3627a61ffd79d3922b384c79b4158f77a7
eisterman <federico.pasqua.96@gmail.com>
2017-07-01 15:09:24 +0200

    Rewritten Demotest with the use of the new command ADD. Sintax Adacted.

--- demoMurPy.py
+++ demoMurPy.py
@@ -1,12 +1,15 @@
 from MurPy import Environment
-from _interfaceobjects import var, put
+from _interfaceobjects import VAR, SET, ADD
 
 
 def main():
-    var("ciao", 5)
-    var("ciao2", 2)
-    var("ciao3", 1)
-    put("ciao", 3)
+    VAR("ciao", 5)
+    VAR("ciao2", 2)
+    VAR("ciao3", 1)
+    SET("ciao", 3)
+    SET("ciao3", ADD("ciao", "ciao2"))
+    VAR("ciao4", 2)
+    # 3 2 5 2
 
 if __name__ == '__main__':
     env = Environment()
