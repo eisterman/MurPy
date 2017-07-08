a3072ec7446f2d013a3b3a2d2929763dbdc70e0a
eisterman <federico.pasqua.96@gmail.com>
2017-07-08 13:57:44 +0200

    Created _controlflowoperations.py It will contain the controlflow operation as IF, ELSE, FOR, WHILE

---
+++ _controlflowoperations.py
@@ -0,0 +1,12 @@
+from _operations import Operation
+
+
+class IFConditionOp(Operation):
+    def __init__(self):
+        pass
+
+    def PreCompile(self, env):
+        pass
+
+    def GetCode(self, env, p):
+        pass
